aee197b83d13a02673dcad55a9dc544cae6dcda9
David <myphonedavid@gmail.com>
2022-11-30 11:17:48 +0100

    Script sh

--- Servicio-IPTV-STREAMING/DNS/config.sh
+++ Servicio-IPTV-STREAMING/DNS/config.sh
@@ -1,7 +1,7 @@
 echo "CONFIG DNS SERVER"
-sudo su
 
 cd /home/labsma/Desktop/Caso-estudio-Servicios-Audiovisuales/Servicio-IPTV-STREAMING/DNS
 cp -r hosts /tmp/hosts
+killall dnsmasq
 /usr/sbin/dnsmasq -C /etc/dnsmasq.d/dnsmasq.conf
 
