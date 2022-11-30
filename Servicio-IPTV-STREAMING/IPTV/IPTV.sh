b7afa46721701104f0e0fa7cb97727faa97b5c0b
David <myphonedavid@gmail.com>
2022-11-30 13:24:15 +0100

    IPTV sh

---
+++ Servicio-IPTV-STREAMING/IPTV/IPTV.sh
@@ -0,0 +1,19 @@
+echo "Config IPTV"
+
+
+mkdir /var/www/apache2
+
+cd /home/labsma/Desktop/Caso-estudio-Servicios-Audiovisuales/Servicio-IPTV-STREAMING/IPTV
+
+cp guia_iptv.xmltv /var/www/apache2
+cp /var/www/index.html /var/www/apache2
+
+cd /etc/apache2/
+mkdir sites-available
+mkdir sites-enabled
+
+cp /home/labsma/Desktop/Caso-estudio-Servicios-Audiovisuales/Servicio-IPTV-STREAMING/IPTV/apache.conf sites-available
+cd sites-available
+a2ensite apache2.conf
+
+service apache2 reload
